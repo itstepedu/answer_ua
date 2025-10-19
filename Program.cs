4fbadaa6a707748b57a93e2e77530526c4e7fb84
Lego <sc.saturn@gmail.com>
2025-10-19 19:00:39 +0300

    view Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //<<<<<<< HEAD
-//    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 //=======
     options.UseSqlite(connectionString));
 
